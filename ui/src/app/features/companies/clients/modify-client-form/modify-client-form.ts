a5f25a3d241cfe5617e7755f0d9a1eb296a2daab
DJAYI <danilo.arenasyi@gmail.com>
2026-01-04 18:35:47 -0500

    feat: integrate AuthService to fetch user email based on company data

--- ui/src/app/features/companies/clients/modify-client-form/modify-client-form.ts
+++ ui/src/app/features/companies/clients/modify-client-form/modify-client-form.ts
@@ -15,6 +15,7 @@ import {
     CompanyCardComponent,
     CompanyCardVariant
 } from "../../shared/components";
+import { AuthService } from "../../../../auth/auth,service";
 
 @Component({
     selector: "app-modify-client-form",
@@ -74,7 +75,7 @@ import {
                     <div class="col-span-1">
                         <app-company-card
                             [displayName]="companyData.displayName"
-                            [email]="companyData.contact.email"
+                            [email]="userEmail()"
                             [variant]="CompanyCardVariant.AGENCY"
                         />
                     </div>
@@ -128,6 +129,9 @@ import {
 export class ModifyClientFormComponent implements OnInit {
     private readonly companyService = inject(CompanyService);
     private readonly route = inject(ActivatedRoute);
+    private readonly authService = inject(AuthService);
+
+    protected readonly userEmail = signal<string>('');
 
     protected readonly CompanyCardVariant = CompanyCardVariant;
 
@@ -165,6 +169,12 @@ export class ModifyClientFormComponent implements OnInit {
         this.companyService.getCompanyById(UserRole.AGENCY, this.companyId).subscribe({
             next: ({ data: companyData }) => {
                 this.company.set(companyData);
+                this.authService.getUsernameByUserId(companyData.userId).subscribe({
+                    next: ({ data }) => {
+                        this.userEmail.set(data);
+                    }
+                });
+
                 this.isLoading.set(false);
             },
             error: (err) => {
