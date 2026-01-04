87dbcae414b996ee66698d09ae5d0ca228365b4c
DJAYI <danilo.arenasyi@gmail.com>
2026-01-04 13:59:23 -0500

    feat: simplify router imports and enhance button hover styles in user popover

--- ui/src/app/layouts/dashboard/header/header-user-popover/header-user-popover.ts
+++ ui/src/app/layouts/dashboard/header/header-user-popover/header-user-popover.ts
@@ -72,8 +72,8 @@ import { AuthService } from "../../../../auth/auth,service";
                                     routerLink="/admin/profile"
                                     type="button"
                                     role="menuitem"
-                                    class="w-full flex items-center text-left gap-2 cursor-pointer px-4 py-1.5 rounded-md hover:bg-red-100 hover:text-red-800 transition-all text-gray-700"
-                                    [routerLinkActive]="'bg-neutral-300 text-black [&>svg]:stroke-black'"
+                                    class="w-full flex items-center text-left gap-2 cursor-pointer px-4 py-1.5 rounded-md hover:bg-red-50 hover:text-red-800 transition-all text-gray-700"
+                                    [routerLinkActive]="'bg-red-200 text-red-600 font-semibold [&>svg]:stroke-red-600'"
                                     >
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="w-5 h-5 stroke-red-700"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M8 7a4 4 0 1 0 8 0a4 4 0 0 0 -8 0" /><path d="M6 21v-2a4 4 0 0 1 4 -4h4a4 4 0 0 1 4 4v2" /></svg>
                                     <span>Perfil</span>
@@ -83,7 +83,7 @@ import { AuthService } from "../../../../auth/auth,service";
                                 <button
                                     type="button"
                                     role="menuitem"
-                                    class="w-full flex items-center text-left gap-2 cursor-pointer px-4 py-1.5 rounded-md hover:bg-red-100 hover:text-red-800 transition-all text-gray-700"
+                                    class="w-full flex items-center text-left gap-2 cursor-pointer px-4 py-1.5 rounded-md hover:bg-red-50 hover:text-red-800 transition-all text-gray-700"
                                 >
                                 <svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="h-5 w-5 stroke-red-700"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10.325 4.317c.426 -1.756 2.924 -1.756 3.35 0a1.724 1.724 0 0 0 2.573 1.066c1.543 -.94 3.31 .826 2.37 2.37a1.724 1.724 0 0 0 1.065 2.572c1.756 .426 1.756 2.924 0 3.35a1.724 1.724 0 0 0 -1.066 2.573c.94 1.543 -.826 3.31 -2.37 2.37a1.724 1.724 0 0 0 -2.572 1.065c-.426 1.756 -2.924 1.756 -3.35 0a1.724 1.724 0 0 0 -2.573 -1.066c-1.543 .94 -3.31 -.826 -2.37 -2.37a1.724 1.724 0 0 0 -1.065 -2.572c-1.756 -.426 -1.756 -2.924 0 -3.35a1.724 1.724 0 0 0 1.066 -2.573c-.94 -1.543 .826 -3.31 2.37 -2.37c1 .608 2.296 .07 2.572 -1.065z" /><path d="M9 12a3 3 0 1 0 6 0a3 3 0 0 0 -6 0" /></svg>
                                     <span>Configuración</span>
